3b541e7df153636aef974b2d8d7dd973a3f8f890
msx98 <git@lx.ax>
2026-02-17 02:19:10 +0200

    dont override uid

--- src/init.sh
+++ src/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export UID=${UID:0}
 [ ! -z "$USER" ] || export USER=$(getent passwd $UID | cut -d: -f1)
 [ ! -z "$HOME" ] || export HOME=$(getent passwd $UID | cut -d: -f6)
 [ ! -z "$XDG_RUNTIME_DIR" ] || export XDG_RUNTIME_DIR=/run/user/$UID
 export MOZ_ENABLE_WAYLAND=1
 export XCURSOR_THEME=Adwaita
 export XCURSOR_SIZE=48
+export GDK_SCALE=2
 export GDK_BACKEND=wayland
 export XCURSOR_PATH=/usr/share/icons
 $@
