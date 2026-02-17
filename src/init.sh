2e2a68fe4b3ffe10ab3ce5845bb2c92c0daccbf7
msx98 <git@lx.ax>
2026-02-17 02:10:39 +0200

    Initial version

---
+++ src/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export UID=${UID:0}
+[ ! -z "$USER" ] || export USER=$(getent passwd $UID | cut -d: -f1)
+[ ! -z "$HOME" ] || export HOME=$(getent passwd $UID | cut -d: -f6)
+[ ! -z "$XDG_RUNTIME_DIR" ] || export XDG_RUNTIME_DIR=/run/user/$UID
+export MOZ_ENABLE_WAYLAND=1
+export XCURSOR_THEME=Adwaita
+export XCURSOR_SIZE=48
+export GDK_BACKEND=wayland
+export XCURSOR_PATH=/usr/share/icons
+$@
+
