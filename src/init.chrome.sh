bfab93697c12ece946e6741eb18d1f12c5fe194d
msx98 <git@lx.ax>
2026-03-12 19:26:24 +0200

    Refactor launch scripts and build process for Chrome and Firefox; add Containerfiles and run configurations

---
+++ src/init.chrome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ ! -z "$USER" ] || export USER=$(getent passwd $UID | cut -d: -f1)
+[ ! -z "$HOME" ] || export HOME=$(getent passwd $UID | cut -d: -f6)
+[ ! -z "$XDG_RUNTIME_DIR" ] || export XDG_RUNTIME_DIR=/run/user/$UID
+export MOZ_ENABLE_WAYLAND=1
+export XCURSOR_THEME=Adwaita
+export XCURSOR_SIZE=48
+export GDK_SCALE=2
+export GDK_BACKEND=wayland
+export XCURSOR_PATH=/usr/share/icons
+
+chown -R $USER:$USER "$HOME"
+chown -R $USER:$USER "$XDG_RUNTIME_DIR"
+su $USER
+
+$@
+
