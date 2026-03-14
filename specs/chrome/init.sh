b44f390117fb68ede9c26593146714784653c04e
msx98 <git@lx.ax>
2026-03-14 13:00:43 +0000

    More proxies

--- specs/chrome/init.sh
+++ specs/chrome/init.sh
@@ -1,5 +1,5 @@
 google-chrome-stable \
 --ozone-platform-hint=auto \
 --ozone-platform=wayland \
---user-data-dir=/home/user/.config/google-chrome \
+--user-data-dir=$HOME/.config/google-chrome \
 --class=Google-chrome
